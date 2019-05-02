a9f1b36de5e1d0cb7797af72c30f86bae5248c25
Chen Huang <alan.tolearn@gmail.com>
2019-05-02 19:48:45 -0400

    Update setting.js

--- src/modules/flappy_bird/setting.js
+++ src/modules/flappy_bird/setting.js
@@ -1,2 +1,15 @@
-export const Width = 600
-export const Height = 400
+// board
+export const Height       = 400
+export const Width        = 600
+export const Interval     = 20
+// bird
+export const BirdHeight   = 20
+export const BirdWidth    = 20
+export const BirdX        = Width / 3
+export const DeltaSpeed   = -250
+export const Gravity      = 800
+// pipe
+export const PipeGap      = 100
+export const PipeWidth    = 50
+export const PipeDistance = 150
+export const PipeSpeed    = 80
